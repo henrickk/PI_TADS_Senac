f874e494663bc4438ef1e3b5fb2da40efbacf600
Leonardo Fujimura <leonardo.fujimura123@gmail.com>
2022-05-20 00:58:36 -0300

    update resume 2.0

--- src/main/java/Boosiness/TesteCurriculo.java
+++ src/main/java/Boosiness/TesteCurriculo.java
@@ -1,18 +1,18 @@
 package Boosiness;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
+
 import static utils.ConsoleColors.*;
 
 public class TesteCurriculo {
 
     //TODO mudar e adicionar nomes
 
-    static String sorteadorNomes(){
+    static String sorteadorNomes() {
 
-        ArrayList<String>listaNomes=new ArrayList<String>();
+        ArrayList<String> listaNomes = new ArrayList<>();
         listaNomes.add("Gorgo");
         listaNomes.add("Liant-me");
         listaNomes.add("Raiiniolam");
@@ -36,17 +36,15 @@ public class TesteCurriculo {
 
         Collections.shuffle(listaNomes);
 
-        String nomeMonstro=listaNomes.get(0);
-
-        return nomeMonstro;
+        return listaNomes.get(0);
 
     }
 
     //TODO mudar e adicionar sobrenomes
 
-    static String sorteadorSobrenomes(){
+    static String sorteadorSobrenomes() {
 
-        ArrayList<String>listaSobrenomes=new ArrayList<String>();
+        ArrayList<String> listaSobrenomes = new ArrayList<>();
         listaSobrenomes.add("Deueray");
         listaSobrenomes.add("Phasbemo");
         listaSobrenomes.add("Abafor");
@@ -80,86 +78,171 @@ public class TesteCurriculo {
 
         Collections.shuffle(listaSobrenomes);
 
-        String sobrenomeMonstro=listaSobrenomes.get(0);
+        return listaSobrenomes.get(0);
 
-        return sobrenomeMonstro;
+    }
 
+    // sorteador de idades
+
+    static int sorteadorIdade() {
+        int max = 450, min = 18;
+
+        Random num = new Random();
+
+        return num.nextInt(max - min) + min;
+
+    }
+
+    //TODO adicionar mais objetivos
+
+    static String sorteadorObjetivos() {
+        ArrayList<String> listaObjetivos = new ArrayList<>();
+        listaObjetivos.add("Aprimorar meus conhecimentos de susto e assustar o máximo de pessoas que eu pudar para ajudar no crescimento da empresa.");
+        listaObjetivos.add("Focar no meu crescimento pessoal e fazer de tudo para me tornar o chefe.");
+        listaObjetivos.add("Assustar todas as pessoas existente para crescer a empresa e aumentar meus conhecimento e habilidades.");
+        listaObjetivos.add("Adquirir novas habilidades e aprimorar as que já tenho.");
+        listaObjetivos.add("Aplicar tudo que aprendi na universidade sustos assustadores.");
+        listaObjetivos.add("Crescer, me aprimorar e focar na evolução da empresa.");
+
+        Collections.shuffle(listaObjetivos);
+
+        return listaObjetivos.get(0);
     }
 
     //TODO adicionar mais característas
 
-    static String sorteadorCaracteristicas(){
+    static String sorteadorCaracteristicas() {
 
-        String  caracteristicasMonstro=" ";
+        String caracteristicasMonstro = " ";
 
-            ArrayList<String> listaCaracteristicas = new ArrayList<String>();
-            listaCaracteristicas.add("Assustador");
-            listaCaracteristicas.add("Grande");
-            listaCaracteristicas.add("Inteligente");
-            listaCaracteristicas.add("Tímido");
-            listaCaracteristicas.add("Estressado");
-            listaCaracteristicas.add("Assustado");
-            listaCaracteristicas.add("Fofo");
+        ArrayList<String> listaCaracteristicas = new ArrayList<>();
+        listaCaracteristicas.add("Assustador");
+        listaCaracteristicas.add("Grande");
+        listaCaracteristicas.add("Inteligente");
+        listaCaracteristicas.add("Tímido");
+        listaCaracteristicas.add("Estressado");
+        listaCaracteristicas.add("Assustado");
+        listaCaracteristicas.add("Fofo");
 
-            Collections.shuffle(listaCaracteristicas);
+        Collections.shuffle(listaCaracteristicas);
 
-            int maximo = 6;
+        int maximo = 6;
 
-            Random aleatorio = new Random();
+        Random aleatorio = new Random();
 
-            int quantidade = aleatorio.nextInt(maximo) +1;
+        int quantidade = aleatorio.nextInt(maximo) + 1;
 
-            switch (quantidade){
-                case 1:
-                    caracteristicasMonstro=listaCaracteristicas.get(0);
-                    break;
-                case 2:
-                    caracteristicasMonstro=listaCaracteristicas.get(0) + ",\n" +
-                    listaCaracteristicas.get(1);
-                    break;
-                case 3:
-                    caracteristicasMonstro=listaCaracteristicas.get(0) + ",\n" +
-                    listaCaracteristicas.get(1) + ",\n" +
-                    listaCaracteristicas.get(2);
-                    break;
-                case 4:
-                    caracteristicasMonstro=listaCaracteristicas.get(0) + ",\n" +
-                    listaCaracteristicas.get(1) + ",\n" +
-                    listaCaracteristicas.get(2) + ",\n" +
-                    listaCaracteristicas.get(3);
-                    break;
-                case 5:
-                    caracteristicasMonstro=listaCaracteristicas.get(0) + ",\n" +
-                    listaCaracteristicas.get(1) + ",\n" +
-                    listaCaracteristicas.get(2) + ",\n" +
-                    listaCaracteristicas.get(3) + ",\n" +
-                    listaCaracteristicas.get(4);
-                    break;
-                case 6:
-                    caracteristicasMonstro=listaCaracteristicas.get(0) + ",\n" +
-                    listaCaracteristicas.get(1) + ",\n" +
-                    listaCaracteristicas.get(2) + ",\n" +
-                    listaCaracteristicas.get(3) + ",\n" +
-                    listaCaracteristicas.get(4) + ",\n" +
-                    listaCaracteristicas.get(5);
+        switch (quantidade) {
+            case 1:
+                caracteristicasMonstro = listaCaracteristicas.get(0);
+                break;
+            case 2:
+                caracteristicasMonstro = listaCaracteristicas.get(0) + ",\n" +
+                        listaCaracteristicas.get(1);
+                break;
+            case 3:
+                caracteristicasMonstro = listaCaracteristicas.get(0) + ",\n" +
+                        listaCaracteristicas.get(1) + ",\n" +
+                        listaCaracteristicas.get(2);
+                break;
+            case 4:
+                caracteristicasMonstro = listaCaracteristicas.get(0) + ",\n" +
+                        listaCaracteristicas.get(1) + ",\n" +
+                        listaCaracteristicas.get(2) + ",\n" +
+                        listaCaracteristicas.get(3);
+                break;
+            case 5:
+                caracteristicasMonstro = listaCaracteristicas.get(0) + ",\n" +
+                        listaCaracteristicas.get(1) + ",\n" +
+                        listaCaracteristicas.get(2) + ",\n" +
+                        listaCaracteristicas.get(3) + ",\n" +
+                        listaCaracteristicas.get(4);
+                break;
+            case 6:
+                caracteristicasMonstro = listaCaracteristicas.get(0) + ",\n" +
+                        listaCaracteristicas.get(1) + ",\n" +
+                        listaCaracteristicas.get(2) + ",\n" +
+                        listaCaracteristicas.get(3) + ",\n" +
+                        listaCaracteristicas.get(4) + ",\n" +
+                        listaCaracteristicas.get(5);
 
-                    break;
-            }
+                break;
+        }
 
         return caracteristicasMonstro;
     }
 
+    //TODO adicionar mais universidades
+
+    static String sorteadorUniversidades() {
+        ArrayList<String> listaFormacao = new ArrayList<>();
+        listaFormacao.add("Universidade Sustos");
+        listaFormacao.add("Universidade Monstros");
+        listaFormacao.add("Centro Universitário Void & Darkness");
+        listaFormacao.add("Faculdade Estatual Sullyvan e Mike");
+        listaFormacao.add("Sustos e Macabros");
+        listaFormacao.add("Centro de formação Escuridão Escura");
+
+        Collections.shuffle(listaFormacao);
+
+        return listaFormacao.get(0);
+    }
+
+    //TODO adicionar mais cursos
+
+    static String sorteadorCurso() {
+        ArrayList<String> listaCurso = new ArrayList<>();
+        listaCurso.add("Administração");
+        listaCurso.add("Ciências do susto");
+        listaCurso.add("Análise e Desenvolvimento de Sistemas");
+        listaCurso.add("Filosofia do Susto");
+        listaCurso.add("Direito");
+        listaCurso.add("Ciências Contábeis");
+        listaCurso.add("Marketing");
+        listaCurso.add("Engenharia do medo");
+        listaCurso.add("Biologia");
+        listaCurso.add("Gestão de Recursos Monstros");
+
+        Collections.shuffle(listaCurso);
+
+        return listaCurso.get(0);
+    }
+
+    // sorteador de nível de formação
+
+    static String nivelFormacao() {
+        ArrayList<String> cursando = new ArrayList<>();
+        cursando.add("Cursando 1º Semestre");
+        cursando.add("Cursando 2º Semestre");
+        cursando.add("Cursando 3º Semestre");
+        cursando.add("Cursando 4º Semestre");
+        cursando.add("Cursando 5º Semestre");
+        cursando.add("Cursando 6º Semestre");
+        cursando.add("Concluído");
+        cursando.add("Interrompido");
+        cursando.add("Incompleto");
+
+        Collections.shuffle(cursando);
+        return cursando.get(0);
+    }
+
     public static void main(String[] args) {
 
-        String nomeMonstro=sorteadorNomes();
-        String sobrenomeMonstro=sorteadorSobrenomes();
-        String caracteristicasMonstro=sorteadorCaracteristicas();
-
         coloredOutput("NOME: ", GREEN);
-        System.out.println(nomeMonstro + " " + sobrenomeMonstro);
-        coloredOutput("\n" + "CARACTERÍSTICAS: ", GREEN);
-        System.out.println(caracteristicasMonstro);
+        System.out.println(sorteadorNomes() + " " + sorteadorSobrenomes());
 
+        coloredOutput("\n" + "IDADE: ", GREEN);
+        System.out.println(sorteadorIdade() + " anos");
+
+        coloredOutput("\n" + "OBJETIVO: ", GREEN);
+        System.out.println(sorteadorObjetivos());
+
+        coloredOutput("\n" + "CARACTERÍSTICAS: ", GREEN);
+        System.out.println(sorteadorCaracteristicas());
+
+        coloredOutput("\n" + "FORMAÇÃO ACADÊMICA: ", GREEN);
+        System.out.println(sorteadorUniversidades());
+        System.out.println(sorteadorCurso() + " - " + nivelFormacao());
 
     }
 
