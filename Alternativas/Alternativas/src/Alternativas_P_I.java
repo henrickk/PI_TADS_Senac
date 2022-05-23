893411d643bf8da61007e5224625fb2f028cd6c9
Henrick Adrian <81784316+henrickk@users.noreply.github.com>
2022-05-22 22:44:44 -0300

    Correção de alguns erros

--- Alternativas/Alternativas/src/Alternativas_P_I.java
+++ Alternativas/Alternativas/src/Alternativas_P_I.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
-import java.util.Random;
 
 public class Alternativas_P_I {
 
@@ -14,10 +13,10 @@ public class Alternativas_P_I {
 
 		boolean correto = false;
 
-		// Peimeira questï¿½o (1)
+		// Peimeira Questionário (1)
 
 		do {
-			System.out.println("1) QUESTï¿½O_1");
+			System.out.println("1) Questão-1");
 			System.out.println("______________________________\n");// 30
 
 			System.out.println("a)Blablabla\n");
@@ -42,7 +41,7 @@ public class Alternativas_P_I {
 			case "A":
 
 				System.out.println("\n------------------------------\n");
-				System.out.println("Parabï¿½ns vocï¿½ acerou! Atï¿½ a prï¿½xima semana!\n");
+				System.out.println("Parabéns você acerou! Até a próxima semana!\n");
 				correto = true;
 				cont = cont + 1;
 				break;
@@ -58,28 +57,33 @@ public class Alternativas_P_I {
 			case "D":
 
 				System.out.println("\n------------------------------\n");// 30
-				System.out.println("Vocï¿½ errou! Tente novamente!");
+				System.out.println("Você errou! Tente novamente!");
 				System.out.println("______________________________\n");// 30
 				cont = cont + 1;
 				break;
+
 			default:
 				System.out.println("______________________________\n");// 30
-				System.out.println("Escolha invï¿½lida!\n");
+				System.out.println("Escolha inválida!\n");
 				System.out.println("______________________________\n");// 30
 				cont = cont + 1;
 			}
+
 		} while (!correto && cont < 3);
 
 		if (correto) {
-			System.out.println("Vocï¿½ acertou na " + cont + " tentativa!");
+			System.out.println("Você acertou na " + cont + " tentativa!");
 			System.out.println("\n______________________________\n");// 30
 		} else {
-			System.out.println("Vocï¿½ errou nas 3 chances!");
+			System.out.println("Você errou nas 3 chances!");
 		}
 
-		/* Prï¿½xima histï¿½ria e em seguida a prï¿½xima questï¿½o (2) */
+		/* Próxima história e em seguida próxima questão (2) */
+		
+		int cont2 = 1;
+		
 		do {
-			System.out.println("2) QUESTï¿½O_2");
+			System.out.println("2) Questionário_2");
 			System.out.println("______________________________\n");// 30
 
 			System.out.println("a)blaBLAbla1\n");
@@ -104,7 +108,7 @@ public class Alternativas_P_I {
 			case "A":
 
 				System.out.println("\n------------------------------\n");
-				System.out.println("Parabï¿½ns vocï¿½ acerou! Atï¿½ a prï¿½xima semana!\n");
+				System.out.println("Parabéns você acerou! Até a próxima semana!\n");
 				correto = true;
 				cont = cont + 1;
 				break;
@@ -120,42 +124,44 @@ public class Alternativas_P_I {
 			case "D":
 
 				System.out.println("\n------------------------------\n");// 30
-				System.out.println("Vocï¿½ errou! Tente novamente!");
+				System.out.println("Você errou! Tente novamente!");
 				System.out.println("______________________________\n");// 30
-				cont = cont + 1;
+				cont = cont2 + 1;
 				break;
+				
 			default:
 				System.out.println("______________________________\n");// 30
-				System.out.println("Resposta invï¿½lida!\n");
-
-				cont = cont + 1;
-				break;
+				System.out.println("Resposta inválida!\n");
+				cont = cont2 + 1;
 			}
-		} while (!correto && cont < 3);
+			
+		} while (!correto && cont2 < 3);
 
 		if (correto) {
-			System.out.println("Vocï¿½ acertou na " + cont + " tentativa!");
+			System.out.println("Você acertou na " + cont2 + " tentativa!");
 			System.out.println("\n______________________________\n");// 30
+			
 		} else {
-			System.out.println("Vocï¿½ errou nas 3 chances!");
+			System.out.println("Você errou nas 3 chances!");
 		}
 
-		/* Prï¿½xima histï¿½ria e em seguida a prï¿½xima questï¿½o (3) BOSS(1) */
+		/* Próxima história e em seguida próxima questão (3) BOSS(1) */
 
 		boolean acertou = false;
 
 		List quest = new ArrayList();
 
-		quest.add("BLA\n");// Questï¿½o correta
+		quest.add("BLA\n");// Questão correta
 		quest.add("BLABLABLA\n");
 		quest.add("BLABLBLAaa\n");
 		quest.add("BLABLABL\n");
+	
 
 		do {
 
 			Collections.shuffle(quest);
 
-			System.out.println("3) Questï¿½o_3 BOSS(1)");
+			System.out.println("3) Questão_3 BOSS(1)");
 			System.out.println("______________________________\n");// 30
 
 			System.out.println("a) " + quest.get(0));
@@ -176,7 +182,7 @@ public class Alternativas_P_I {
 			switch (resposta) {
 			case "a":
 			case "A":
-				if (quest.get(0).equals("BLA")) {
+				if (quest.get(0).equals("BLA\n")) {
 					System.out.println("Resposta correta!");
 					acertou = true;
 				} else {
@@ -184,9 +190,10 @@ public class Alternativas_P_I {
 					System.out.println("______________________________\n");// 30
 				}
 				break;
+				
 			case "b":
 			case "B":
-				if (quest.get(1).equals("BLA")) {
+				if (quest.get(1).equals("BLA\n")) {
 					System.out.println("Resposta correta!");
 					acertou = true;
 				} else {
@@ -194,9 +201,10 @@ public class Alternativas_P_I {
 					System.out.println("______________________________\n");// 30
 				}
 				break;
+				
 			case "c":
 			case "C":
-				if (quest.get(2).equals("BLA")) {
+				if (quest.get(2).equals("BLA\n")) {
 					System.out.println("Resposta correta!");
 					acertou = true;
 				} else {
@@ -204,9 +212,10 @@ public class Alternativas_P_I {
 					System.out.println("______________________________\n");// 30
 				}
 				break;
+				
 			case "d":
 			case "D":
-				if (quest.get(3).equals("BLA")) {
+				if (quest.get(3).equals("BLA\n")) {
 					System.out.println("Resposta correta!");
 					acertou = true;
 				} else {
@@ -214,8 +223,10 @@ public class Alternativas_P_I {
 					System.out.println("______________________________\n");// 30
 				}
 				break;
+				
+				
 			default:
-				System.out.println("Resposta invï¿½lida!");
+				System.out.println("Resposta inválida!");
 				System.out.println("______________________________\n");// 30
 			}
 
